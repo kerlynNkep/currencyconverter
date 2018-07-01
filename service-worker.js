385d314b7d1b32b8d9a58eaecf02d8afe45ed76e
kerlynNkep <kerlynmanyi@gmail.com>
2018-07-01 22:00:26 +0100

    cache files stored

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
 self.addEventListener( "install" , function (event) { 
     console.log('Installing service worker.'); 
     event.waitUntil(
-        cache.open('currency_convert_v1').then(cache =>{
-            console.log('Caching app shell...');
+        caches.open('currency_convert_v1').then(cache => {
+                console.log('Caching app shell...');
                 return cache.addAll(
                     [
-                        
+
                         "/index.html",
                 	    "/service-worker.js",
                	 	    "/js/jquery.min.js",
                 	    "/css/bootstrap.css",
                 	    "/css/main.css",
                         "/js/converter.js",
-                        "/image/images.png"
+                        "/images/images.png"
                     ]
                 );
             }).catch( error => console.log('failed to cache: ' + error))
